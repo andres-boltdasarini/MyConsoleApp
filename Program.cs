179f4c8978870c2c77cc9133c4542433854afa61
akupriyanov <akupriyanov@astralinux.ru>
2025-02-25 10:07:17 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+class MainClass
+{
+    public static void Main(string[] args)
+    {
+        int[,] arr = { { -5, 6, 9, 1, 2, -3 }, { -8, 8, 1, 1, 2, -3 } };
+
+        // Проходим по каждой строке массива
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            // Сортировка элементов в текущей строке
+            for (int j = 0; j < arr.GetLength(1) - 1; j++)
+            {
+                for (int k = j + 1; k < arr.GetLength(1); k++)
+                {
+                    if (arr[i, j] > arr[i, k])
+                    {
+                        // Меняем местами элементы, если они находятся в неправильном порядке
+                        int temp = arr[i, j];
+                        arr[i, j] = arr[i, k];
+                        arr[i, k] = temp;
+                    }
+                }
+            }
+        }
+
+        // Вывод отсортированного массива
+        for (int i = 0; i < arr.GetLength(0); i++)
+        {
+            for (int j = 0; j < arr.GetLength(1); j++)
+            {
+                Console.Write(arr[i, j] + " ");
+            }
+            Console.WriteLine();
+        }
+    }
+}
