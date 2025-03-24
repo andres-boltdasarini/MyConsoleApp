1e74eb8306b5e99439c57dfa5ebec3c5f71cf445
Андрей <lau47@mail.ru>
2025-03-24 13:49:21 +0300

    operator_7212

--- Program.cs
+++ Program.cs
@@ -1,53 +1,41 @@
-﻿class A
+﻿class Obj
 {
-    public virtual void Display()
-    {
-        Console.WriteLine("Метод класса A");
-    }
-}
+    public int Value;
 
-class B : A
-{
-    public new void Display()
+    public static Obj operator +(Obj a, Obj b)
     {
-        Console.WriteLine("Метод класса B");
+        return new Obj
+        {
+            Value = a.Value + b.Value
+        };
     }
-}
-
-class C : A
-{
-    public override void Display()
+    public static Obj operator -(Obj a, Obj b)
     {
-        Console.WriteLine("Метод класса C");
-    }
-}
-
-class D : B
-{
-    public new void Display()
-    {
-        Console.WriteLine("Метод класса D");
-    }
-}
-
-class E : C
-{
-    public new void Display()
-    {
-        Console.WriteLine("Метод класса E");
+        return new Obj
+        {
+            Value = a.Value - b.Value
+        };
     }
 }
 class Program
 {
     static void Main()
     {
-        D d = new D();
-        E e = new E();
+        // Создаем два объекта класса Obj
+        Obj obj1 = new Obj { Value = 10 };
+        Obj obj2 = new Obj { Value = 5 };
 
-        d.Display();
-        ((A)e).Display();
-        ((B)d).Display();
-        ((A)d).Display();
+        // Используем перегруженный оператор +
+        Obj sum = obj1 + obj2;
+        Console.WriteLine($"obj1 + obj2 = {sum.Value}"); // Выведет: 15
 
+        // Используем перегруженный оператор -
+        Obj difference = obj1 - obj2;
+        Console.WriteLine($"obj1 - obj2 = {difference.Value}"); // Выведет: 5
+
+        // Можно также делать цепочки операций
+        Obj obj3 = new Obj { Value = 3 };
+        Obj result = obj1 + obj2 - obj3;
+        Console.WriteLine($"obj1 + obj2 - obj3 = {result.Value}"); // Выведет: 12
     }
 }
