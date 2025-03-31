3df2a1ede9cbedab26bed9195e8d849ac06dc920
Андрей <lau47@mail.ru>
2025-03-31 11:18:08 +0300

    DIrectory_824

--- Program.cs
+++ Program.cs
@@ -1,170 +1,79 @@
-﻿static class OrderCalculator
+﻿using System;
+using System.Runtime;
+
+namespace DirectoryManager
 {
-    public static int CostCalculate(Product[] products)
+    class Program
     {
-        int total = 0;
-        for (int i = 0; i < products.Length; i++)
+        static void Main(string[] args)
         {
-            total += products[i].Cost;
+            GetCatalogs(); //   Вызов метода получения директорий
+            Getall();
+            Desktop();
         }
-        return total;
-    }
-}
 
-
-abstract class Delivery
-{
-    public class Naming<Tid>
-    {
-        public Tid? Id { get; private set; }
-        public void SetId(Tid id) => Id = id;
-    }
-
-    public Adress address = new();
-    public Phone phones = new Phone { PhoneNumber = 456 };
-}
-
-class HomeDelivery : Delivery
-{
-    public void SetCourierName(string courierName) { CourierName.SetId(courierName); }
-    public Naming<String> CourierName = new Naming<String>();
-    public HomeDelivery(string courierName) { CourierName.SetId(courierName); }
-
-}
-
-class PickPointDelivery : Delivery
-{
-    public void SetPointNumber(int pickPointNumber) { PickPointNumber.SetId(pickPointNumber); }
-    public Naming<int> PickPointNumber = new Naming<int>();
-    public PickPointDelivery(int pickPointNumber) { PickPointNumber.SetId(pickPointNumber); }
-}
-
-class ShopDelivery : Delivery
-{
-    public void SetShopName(string shopName) { ShopName.SetId(shopName); }
-    public Naming<String> ShopName = new Naming<String>();
-    public ShopDelivery(string shopName) { ShopName.SetId(shopName); }
-
-}
-static class ExtentonOrder
-{
-    public static int SetNumber(this int num)
-    {
-        num = 2;
-        return num;
-    }
-    public static Product SetProd(this Product prod)
-    {
-        int obj = 1;
-        prod.SetData("netbook", 345, obj);
-        return prod;
-    }
-}
-class Order<TDelivery, TStruct> where TDelivery : Delivery
-{
-    public TDelivery? Delivery { get; set; }
-    public TStruct? Struct { get; set; }
-    public int number;
-    public string? description;
-    public Product[]? products;
-    private int cost;
-    public int Cost => cost;
-    public Product this[int index]
-    {
-        get
+        static void GetCatalogs()
         {
-            if (index >= 0 && index < products.Length)
+            string dirName = @"C:\\"; // Прописываем путь к корневой директории MacOS (для Windows скорее всего тут будет "C:\\")
+            if (Directory.Exists(dirName)) // Проверим, что директория существует
             {
-                return products[index];
-            }
-            else
-            {
-                return null;
-            }
+                Console.WriteLine("Папки:");
+                string[] dirs = Directory.GetDirectories(dirName);  // Получим все директории корневого каталога
 
-        }
-        set
-        {
-            if (index >= 0 && index < products.Length)
-            {
-                products[index] = value;
+                foreach (string d in dirs) // Выведем их все
+                    Console.WriteLine(d);
+
+                Console.WriteLine();
+                Console.WriteLine("Файлы:");
+                string[] files = Directory.GetFiles(dirName);// Получим все файлы корневого каталога
+                int sum = files.Length + dirs.Length;
+                foreach (string s in files)   // Выведем их все
+                    Console.WriteLine(s);
+                Console.WriteLine($"количество обьектов: {sum}");
+                
+
             }
         }
-    }
+        static public void Getall()
+        {
 
-    public void DisplayAdress()
-    {
-        if (Delivery.address != null)
-            Console.WriteLine(Delivery.address.AdressName);
-        else
-            Console.WriteLine("Address not set.");
-    }
-    public int CalculateCost()
-    {
-        cost = OrderCalculator.CostCalculate(products);
-        number.SetNumber();
-        products[0].SetProd();
-        return cost;
-    }
-}
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(@"/" /* Или С:\\ для Windows */ );
+                if (dirInfo.Exists)
+                {
+                    Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+                }
 
-abstract class Product
-{
-    protected string? name;
-    public int Cost { get; private set; }
+                DirectoryInfo newDirectory = new DirectoryInfo(@"/newDirectory");
+                if (!newDirectory.Exists)
+                    newDirectory.Create();
 
-    public virtual void SetData(string name, int cost, object data)
-    {
-        this.name = name;
-        Cost = cost;
-    }
-}
+                Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+                newDirectory.Delete(true);
+                Console.WriteLine(dirInfo.GetDirectories().Length + dirInfo.GetFiles().Length);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
 
-class Computer : Product
-{
-    public string? processorName { get; private set; }
-    public override void SetData(string name, int cost, object data)
-    {
-        base.SetData(name, cost, data);
-        if (data != null && data is string) processorName = (string)data;
-        else processorName = "empty";
-    }
-}
 
-class Motherboard
-{
-    private Computer computer;
 
-    public Motherboard()
-    {
-        computer = new Computer();
-    }
-}
-class Memory
-{
-    private Computer computer;
-    public Memory(Computer computer) => this.computer = computer;
-}
-class Adress
-{
-    public string? AdressName { get; private set; }
-}
+        }
+        static public void Desktop()
+        {
+            try
+            {
+                DirectoryInfo dirInfo = new DirectoryInfo(@"/Users/luft/Desktop/testFolder");
+                string trashPath = "/Users/luft/.Trash/testFolder";
 
-class Phone
-{
-    public int PhoneNumber;
-}
-class Box
-{
-    public int Weight { get; }
-
-    public Box(int weight)
-    {
-        Weight = weight;
-    }
-
-    public static Box operator +(Box a, Box b)
-    {
-        return new Box(a.Weight + b.Weight);
+                dirInfo.MoveTo(trashPath);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
     }
 }
