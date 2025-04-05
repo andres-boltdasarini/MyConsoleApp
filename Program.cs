0a041da2c640f1128ec315defb83940f097e3f2f
Андрей <lau47@mail.ru>
2025-04-05 12:15:09 +0300

    fin_962

--- Program.cs
+++ Program.cs
@@ -1,31 +1,15 @@
 ﻿using System;
 
-public class MyCustomException : Exception
-{
-    private string _message;
-
-    public MyCustomException(string message)
-    {
-        _message = message;
-    }
-
-    public override string Message
-    {
-        get { return _message; }
-    }
-
-}
-
 public class Program
 {
     public static void Main()
     {
-        Exception[] exceptions = new Exception[] { 
-            new MyCustomException("my"),
-            new ArgumentNullException("not null"),
-            new FileNotFoundException("file not found", "file.txt"),
-            new FormatException("err data"),
-            new OverflowException("arph over")
+        Exception[] exceptions = new Exception[] {
+            new MyCustomException("Пользовательская ошибка"),
+            new ArgumentNullException("Ошибка аргумента"),
+            new FileNotFoundException("Ошибка файла", "file.txt"),
+            new PathTooLongException("Ошибка пути"),
+            new OverflowException("Ошибка переполнения")
         };
 
         foreach (var exception in exceptions)
@@ -46,7 +30,7 @@ public class Program
             {
                 Console.WriteLine($"text3: {ex.Message}, text3: {ex.FileName}");
             }
-            catch (FormatException ex)
+            catch (PathTooLongException ex)
             {
                 Console.WriteLine($"text4: {ex.Message}");
             }
@@ -56,6 +40,76 @@ public class Program
             }
 
         }
+        NumberReader numberReader = new NumberReader();
+        numberReader.NumberEnteredEvent += ShowNumber;
 
+        while (true)
+        {
+            try
+            {
+                numberReader.Read();
+            }
+            catch (MyCustomException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Ошибка: введите число!");
+            }
+        }
+
+        static void ShowNumber(int number, List<string> surnames)
+        {
+            Console.WriteLine("\nРезультат сортировки:");
+            switch (number)
+            {
+                case 1:
+                    surnames.Sort();
+                    break;
+                case 2:
+                    surnames.Sort();
+                    surnames.Reverse();
+                    break;
+            }
+            foreach (var surname in surnames)
+            {
+                Console.WriteLine(surname);
+            }
+        }
+    }
+}
+
+public class MyCustomException : FormatException
+{
+    private string _message;
+
+    public MyCustomException(string message)
+    {
+        _message = message;
+    }
+    public override string Message
+    {
+        get { return _message; }
+    }
+}
+class NumberReader
+{
+    public delegate void NumberEnteredDelegate(int number, List<string> surnames);
+    public event NumberEnteredDelegate NumberEnteredEvent;
+    private List<string> surnames = new List<string> { "Иванов", "Петров", "Сидоров", "Абрамов", "Козлов" };
+
+    public void Read()
+    {
+        Console.WriteLine("\nВведите 1 для сортировки А-Я или 2 для Я-А");
+        int number = Convert.ToInt32(Console.ReadLine());
+        if (number != 1 && number != 2)
+        throw new MyCustomException("Ошибка: введите только 1 или 2");
+        NumberEntered(number);
+    }
+
+    protected virtual void NumberEntered(int number)
+    {
+        NumberEnteredEvent?.Invoke(number, surnames);
     }
 }
