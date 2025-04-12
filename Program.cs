c570b60e52011d3d3a3e6663e820ef7103329d42
Андрей <lau47@mail.ru>
2025-04-12 12:56:48 +0300

    fin_1051

--- Program.cs
+++ Program.cs
@@ -10,63 +10,27 @@ public class Logger : ILogger
 {
     public void Event(string message)
     {
-        Console.BackgroundColor = ConsoleColor.Red;
+        Console.BackgroundColor = ConsoleColor.Blue;
         Console.ForegroundColor = ConsoleColor.Black;
         Console.WriteLine(message);
     }
     public void Error(string message)
     {
+        Console.BackgroundColor = ConsoleColor.Red;
+        Console.ForegroundColor = ConsoleColor.Black;
         Console.WriteLine(message);
     }
 }
-public interface IWorker
-{
-    void Work();
-}
-public class Worker1: IWorker
-{
-    ILogger Logger { get; }
-    public Worker1(ILogger logger)
-    {
-        Logger = logger;
-    }
 
-    public void Work()
-    {
-        Logger.Event("Worker1 начал свою работу");
-        Thread.Sleep(3000);
-        Logger.Event("Worker1 закончил свою работу");
-    }
-}
-public class Worker2 : IWorker
+interface ICalculator
 {
-    ILogger Logger { get; }
-    public Worker2(ILogger logger)
-    {
-        Logger = logger;
-    }
-
-    public void Work()
-    {
-        Logger.Event("Worker2 начал свою работу");
-        Thread.Sleep(3000);
-        Logger.Event("Worker2 закончил свою работу");
-    }
+    public double Add(double a, double b);
 }
-public class Worker3 : IWorker
-{
-    ILogger Logger { get; }
-    public Worker3(ILogger logger)
-    {
-        Logger = logger;
-    }
 
-    public void Work()
-    {
-        Logger.Event("Worker3 начал свою работу");
-        Thread.Sleep(3000);
-        Logger.Event("Worker3 закончил свою работу");
-    }
+class Calculator : ICalculator
+{
+    public double Add(double a, double b) => a + b;
+
 }
 
 class Program
@@ -75,12 +39,43 @@ class Program
     static void Main()
     {
         Logger = new Logger();
-        var worker1 = new Worker1(Logger);
-        var worker2 = new Worker2(Logger);
-        var worker3 = new Worker3(Logger);
-        worker1.Work();
-        worker2.Work();
-        worker3.Work();
-        Console.ReadKey();
+
+
+        ICalculator calculator = new Calculator();
+        double a = GetValidNumber("первое", Logger);
+        double b = GetValidNumber("второе", Logger);
+        double sum = calculator.Add(a, b);
+        Logger.Event($"сумма {sum}");
+    }
+
+    static double GetValidNumber(string prompt, ILogger logger)
+    {
+        while (true)
+        {
+            try
+            {
+                Logger.Event($"Введите {prompt} число"); 
+                return GetNumberFromUser();
+            }
+            catch (FormatException)
+            {
+                Logger.Error("Число не корректно");
+            }
+            catch (Exception ex)
+            {
+                Logger.Error(ex.Message);
+            }
+        }
+
+    }
+
+    static double GetNumberFromUser()
+    {
+        string? input = Console.ReadLine();
+        if (double.TryParse(input, out double number))
+        {
+            return number;
+        }
+        else throw new FormatException();
     }
 }
