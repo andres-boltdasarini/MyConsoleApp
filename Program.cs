0d42efb21068ea92f08c94c11b74a446d7601396
akupriyanov <akupriyanov@astralinux.ru>
2025-05-13 16:06:26 +0300

    LINQ_extension_SelectMany

--- Program.cs
+++ Program.cs
@@ -9,38 +9,63 @@ namespace LinqTest
       
        static void Main(string[] args)
        {
-           // Добавим Россию с её городами
-           var russianCities = new List<City>();
-           russianCities.Add(new City("Москва", 11900000));
-           russianCities.Add(new City("Санкт-Петербург", 4991000));
-           russianCities.Add(new City("Волгоград", 1099000));
-           russianCities.Add(new City("Казань", 1169000));
-           russianCities.Add(new City("Севастополь", 449138));
-
-  //          var bigCities = from russianCity in russianCities
-  //  where russianCity.Population > 1000000
-  //  orderby russianCity.Population descending
-  //  select russianCity;
-  //А теперь попробуйте выбрать все города, название у которых не длиннее 10 букв, и отсортируйте их по длине названия.
-
-  var bigCities = russianCities.Where(c => c.Name.Length < 11)
-  .OrderByDescending(c => c.Name.Length);
+// Словарь для хранения стран с городами
+var Countries = new Dictionary<string, List<City>>();
  
-foreach (var bigCity in bigCities)
-   Console.WriteLine(bigCity.Name + " - " + bigCity.Population);
+// Добавим Россию с её городами
+var russianCities = new List<City>();
+russianCities.Add(new City("Москва", 11900000));
+russianCities.Add(new City("Санкт-Петербург", 4991000));
+russianCities.Add(new City("Волгоград", 1099000));
+russianCities.Add(new City("Казань", 1169000));
+russianCities.Add(new City("Севастополь", 449138));
+Countries.Add("Россия", russianCities);
+ 
+// Добавим Беларусь
+var belarusCities = new List<City>();
+belarusCities.Add(new City("Минск", 1200000));
+belarusCities.Add(new City("Витебск", 362466));
+belarusCities.Add(new City("Гродно", 368710));
+Countries.Add("Беларусь", belarusCities);
+ 
+// Добавим США
+var americanCities = new List<City>();
+americanCities.Add(new City("Нью-Йорк", 8399000));
+americanCities.Add(new City("Вашингтон", 705749));
+americanCities.Add(new City("Альбукерке", 560218));
+Countries.Add("США", americanCities);  
+
+// var cities = from country in Countries // пройдемся по странам
+//                from city in country.Value // пройдемся по городам
+//                where city.Population > 1000000 // выберем города-миллионники
+//                orderby city.Population descending // отсортируем по населению
+//                select city;
+
+var cities = Countries
+    .SelectMany(country => country.Value)  // пройдемся по городам всех стран
+    .Where(city => city.Population > 1000000)  // выберем города-миллионники
+    .OrderByDescending(city => city.Population);  // отсортируем по населению
+ 
+foreach (var city in cities)
+   Console.WriteLine(city.Name + " - " + city.Population);
        }
  
-       // Создадим модель класс для города
-       public class City
-       {
-           public City(string name, long population)
-           {
-               Name = name;
-               Population = population;
-           }
-          
-           public string Name { get; set; }
-           public long Population { get; set; }
-       }
+
+        // Создадим модель класс для города
+        public class City {
+          public City(string name, long population) {
+            Name = name;
+            Population = population;
+          }
+
+          public string Name {
+            get;
+            set;
+          }
+          public long Population {
+            get;
+            set;
+          }
+        }
    }
 }
