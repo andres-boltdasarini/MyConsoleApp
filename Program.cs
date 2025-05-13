fe0724c577e17d26210f4655f4baa8b8d3a347de
akupriyanov <akupriyanov@astralinux.ru>
2025-05-13 18:00:37 +0300

    SelectMany2

--- Program.cs
+++ Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿﻿using System;
 using System.Collections.Generic;
 using System.Linq;
  
@@ -6,19 +6,38 @@ namespace LinqTest
 {
    class Program
    {
+      
        static void Main(string[] args)
        {
-string[] text = { "Раз два три",
-   "четыре пять шесть",
-   "семь восемь девять" };
-
-var words = from str in text // пробегаемся по элементам массива
-   from word in str.Split(' ') // дробим каждый элемент по пробелам, сохраняя в новую последовательность
-   select word; // собираем все куски в результирующую выборку
- 
-// выводим результат
-foreach (var word in words)
-   Console.WriteLine(word);
-       }
+// Подготовим данные
+List<Student> students = new List<Student>
+{
+   new Student {Name="Андрей", Age=23, Languages = new List<string> {"английский", "немецкий" }},
+   new Student {Name="Сергей", Age=27, Languages = new List<string> {"английский", "французский" }},
+   new Student {Name="Дмитрий", Age=29, Languages = new List<string> {"английский", "испанский" }},
+   new Student {Name="Василий", Age=24, Languages = new List<string> {"испанский", "немецкий" }}
+};
+          
+// Составим запрос ()
+var selectedStudents = students.SelectMany(
+       // коллекция, которую нужно преобразовать
+       s => s.Languages,
+       // функция преобразования, применяющаяся к каждому элементу коллекции
+       (s, l) => new { Student = s, Lang = l })
+       // дополнительные условия                          
+   .Where(s => s.Lang == "английский" && s.Student.Age < 28)
+       // указатель на объект выборки
+   .Select(s=>s.Student);
+          
+// Выведем результат
+foreach (Student student in selectedStudents)
+   Console.WriteLine($"{student.Name} - {student.Age}");
+        }
    }
+       class Student
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public List<string> Languages { get; set; }
+    }
 }
