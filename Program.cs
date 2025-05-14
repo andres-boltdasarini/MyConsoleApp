25eb4fba977566269efcc1faf82902f195767ef0
akupriyanov <akupriyanov@astralinux.ru>
2025-05-14 15:23:04 +0300

    1416_SelectMany3

--- Program.cs
+++ Program.cs
@@ -10,20 +10,22 @@ namespace LinqTest
        static void Main(string[] args)
        {
 // Подготовим данные
-var companies = new Dictionary<string, string[]>( );
- 
-companies.Add("Apple", new []{ "Mobile",  "Desktop"  });
-companies.Add("Samsung", new []{ "Mobile"} );
-companies.Add("IBM", new []{ "Desktop" } ); 
+var numsList = new List<int[]>()
+{
+   new[] {2, 3, 7, 1},
+   new[] {45, 17, 88, 0},
+   new[] {23, 32, 44, -6},
+};
           
 // Составим запрос ()
-var mobileCompanies = companies
+var mobileCompanies = numsList
 //.Where(companie => companie.value == "Mobile");
 
-               .Where(c => c.Value.Contains("Mobile"));
+               .SelectMany(c => c)
+                  .OrderBy(c => c);
  
            foreach (var company in mobileCompanies)
-               Console.WriteLine(company.Key);
+               Console.WriteLine(company);
 
 //foreach (var city in prodMob)
   // Console.WriteLine(city);
