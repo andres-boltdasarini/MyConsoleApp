ef6ad2b516d3e299891d46ce718b8927100e49a9
akupriyanov <akupriyanov@astralinux.ru>
2025-06-03 13:22:35 +0300

    1721_IMonsterFactory

--- Program.cs
+++ Program.cs
@@ -1,32 +1,117 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
- 
-namespace Task1
+﻿namespace AbstractFactoryRealExample
 {
    class Program
    {
        static void Main(string[] args)
        {
-           var classes = new []
-           {
-               new Classroom { Students = {"Evgeniy", "Sergey", "Andrew"}, },
-               new Classroom { Students = {"Anna", "Viktor", "Vladimir"}, },
-               new Classroom { Students = {"Bulat", "Alex", "Galina"}, }
-           };
-           var allStudents = GetAllStudents(classes);
-         
-           Console.WriteLine(string.Join(" ", allStudents));
-       }
+           // Создание дракона через фабрику
+           var dragon = new Monster(new DragonFactory());
+           dragon.Move();
+           dragon.Hit();
  
-       static string [] GetAllStudents( Classroom [] classes )
-       {
-           return classes.SelectMany(c => c.Students).ToArray();
+           Console.WriteLine();
+           // Создание орка через фабрику
+           var orc = new Monster(new OrcFactory());
+           orc.Move();
+           orc.Hit();
+ 
+           Console.WriteLine();
+           Console.WriteLine("Всем конец...");
        }
-      
-       public class Classroom
+   }
+
+   interface IMovement
+   {
+       void Start();
+   }
+
+      class RunMovement : IMovement
+   {
+       public void Start()
        {
-           public List<string> Students = new List<string>();
+           Console.WriteLine("Бежим");
+       }
+   }
+
+      class FlyMovement : IMovement
+   {
+       public void Start()
+       {
+           Console.WriteLine("Летим");
+       }
+   }
+
+      interface IWeapon
+   {
+       void Attack();
+   }
+
+      class Axe : IWeapon
+   {
+       public void Attack()
+       {
+           Console.WriteLine("Бьем топором");
+       }
+   }
+
+      class FireBreath : IWeapon
+   {
+       public void Attack()
+       {
+           Console.WriteLine("Дышим огнем");
+       }
+   }
+
+      interface IMonsterFactory
+   {
+        IMovement CreateMovement();
+        IWeapon CreateWeapon();
+   }
+
+      class DragonFactory : IMonsterFactory
+   {
+       public IMovement CreateMovement()
+       {
+           return new FlyMovement();
+       }
+       public IWeapon CreateWeapon()
+       {
+           return new FireBreath();
+       }
+   }
+
+      class OrcFactory : IMonsterFactory
+   {
+       public IMovement CreateMovement()
+       {
+           return new RunMovement();
+       }
+       public IWeapon CreateWeapon()
+       {
+           return new Axe();
+       }
+   }
+
+   class Monster
+   {
+       private IWeapon _weapon;
+       private IMovement _movement;
+      
+       /// <summary>
+       ///  Метод - конструктор, где создаются объекты при помощи фабрики
+       /// </summary>
+       public Monster(IMonsterFactory factory)
+       {
+           _weapon = factory.CreateWeapon();
+           _movement = factory.CreateMovement();
+       }
+       public void Move()
+       {
+           _movement.Start();
+       }
+       public void Hit()
+       {
+           _weapon.Attack();
        }
    }
 }
