71566a33881ca5f060e436cab8224d7b8fe90bd2
akupriyanov <akupriyanov@astralinux.ru>
2025-06-10 15:50:56 +0300

    175_SingleResponsibility

--- Program.cs
+++ Program.cs
@@ -1,121 +1,49 @@
-﻿   interface IObserver
-   {
-       void Update(Object o);
-   }
+﻿interface IExporter
+{
+   void Export(string text);
+}
 
-      class StockData
+class PdfExporter : IExporter
+{
+   public void Export(string text)
    {
-       public int USD { get; set; }
-       public int Euro { get; set; }
+       Console.WriteLine($"{text}  => Экспортировано в PDF");
    }
+}
 
-      interface IObservable
+class Document
+{
+   public string Text { get; set; }
+  
+   public void ScrollUp()
    {
-       // добавить наблюдателя
-       void RegisterObserver(IObserver o);
-      
-       // удалить наблюдателя
-       void RemoveObserver(IObserver o);
-      
-       // уведомление наблюдателей
-       void NotifyObservers();
-   }
-
-      class Bank : IObserver
-   {
-       IObservable stock;
-       public Bank(IObservable obs)
-       {
-           stock = obs;
-           stock.RegisterObserver(this);
-       }
-      
-       public void Update(object o)
-       {
-           StockData sData = (StockData)o;
-   
-           if (sData.Euro > 85)
-               Console.WriteLine($"Банк продает евро по курсу {sData.Euro}");
-           else
-               Console.WriteLine($"Банк покупает евро по курсу {sData.Euro}");
-       }
-   }
-
-      class Broker : IObserver
-   {
-       IObservable stock;
+       Console.WriteLine($"Прокрутка вверх");   }
  
-       public Broker(IObservable obs)
-       {
-           stock = obs;
-           stock.RegisterObserver(this);
-       }
- 
-       public void Update(object o)
-       {
-           StockData sData = (StockData) o;
- 
-           if (sData.USD > 85)
-               Console.WriteLine($"Брокер продает доллары  по курсу {sData.USD}");           else
-               Console.WriteLine($"Брокер покупает доллары  по курсу {sData.USD}");       }
- 
-       public void StopTrade()
-       {
-           stock.RemoveObserver(this);
-           stock = null;
-       }
-   }
-
-   class Stock : IObservable
+   public void ScrollDown()
    {
-       /// <summary>
-       /// Информация о торгах
-       /// </summary>
-       StockData _sData;
-       List<IObserver> observers;
-       public Stock()
-       {
-           observers = new List<IObserver>();
-           _sData = new StockData();
-       }
-       public void RegisterObserver(IObserver o)
-       {
-           observers.Add(o);
-       }
-       public void RemoveObserver(IObserver o)
-       {
-           observers.Remove(o);
-       }
-       public void NotifyObservers()
-       {
-           foreach(IObserver o in observers)
-           {
-               o.Update(_sData);
-           }
-       }
-       public void Market()
-       {
-           Random rnd = new Random();
-           _sData.USD = rnd.Next(70, 90);
-           _sData.Euro = rnd.Next(80, 100);
-           NotifyObservers();
-       }
-   }
-
-      class Program
+       Console.WriteLine($"Прокрутка вниз");   }
+ 
+   public void ZoomIn()
    {
-       static void Main(string[] args)
+       Console.WriteLine("Увеличиваем масштаб");
+   }
+ 
+   /// <summary>
+   ///  Экспорт в любые форматы
+   /// </summary>
+   public void Export(IExporter exporter)
+   {
+       exporter.Export(Text);
+   }
+}
+
+   class Program
+   {
+       static void Main()
        {
-           Stock stock = new Stock();
-          
-           var bank = new Bank(stock);
-           var broker = new Broker(stock);
-          
-           // имитация торгов
-           stock.Market();
-           // брокер прекращает наблюдать за торгами
-           broker.StopTrade();
-           // имитация торгов
-           stock.Market();
+           IExporter exporter = new PdfExporter();
+           Document doc = new Document();
+           doc.Text = "Hello World";
+           doc.Export(exporter);
        }
    }
