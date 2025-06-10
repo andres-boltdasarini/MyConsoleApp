c2294aa52ad6ef860b63452ada0013f32a4700ae
akupriyanov <akupriyanov@astralinux.ru>
2025-06-10 11:53:08 +0300

    1741_State

--- Program.cs
+++ Program.cs
@@ -1,64 +1,82 @@
-﻿abstract class NotificationHandler {
-  public NotificationHandler Successor { get; set; }
-  public abstract void Handle(Receiver receiver);
-}
-
-class EmailNotificationHandler: NotificationHandler {
-  public override void Handle(Receiver receiver) {
-    if (receiver.EmailNotification == true)
-      Console.WriteLine("Выполнено уведомление по e-mail");
-    else if (Successor != null)
-      Successor.Handle(receiver);
-  }
-}
-
-class SmsNotificationHandler: NotificationHandler {
-  public override void Handle(Receiver receiver) {
-    if (receiver.SmsNotification == true)
-      Console.WriteLine("Выполнено уведомление по SMS");
-    else if (Successor != null)
-      Successor.Handle(receiver);
-  }
-}
-
-class VoiceNotificationHandler: NotificationHandler {
-  public override void Handle(Receiver receiver) {
-    if (receiver.VoiceNotification == true)
-      Console.WriteLine("Выполнено уведомление по телефону");
-    else if (Successor != null)
-      Successor.Handle(receiver);
-  }
-}
-
-   class Receiver
+﻿   interface IElevatorState
    {
-       // банковские переводы
-       public bool EmailNotification { get; set; }
-       // денежные переводы - WesternUnion, Unistream
-       public bool SmsNotification { get; set; }
-       // перевод через PayPal
-       public bool VoiceNotification { get; set; }
-       public Receiver(bool emailNotification, bool smsNotification, bool voiceNotification)
-       {
-           EmailNotification = emailNotification;
-           SmsNotification = smsNotification;
-           VoiceNotification = voiceNotification;
-       }
+       void Up(Elevator elevator);
+       void Down(Elevator elevator);
    }
 
-      class Program
+   class LowerElevatorState : IElevatorState
+{
+   public void Up(Elevator elevator)
+   {
+       Console.WriteLine("Поднимаемся на первый этаж");
+       elevator.ElevatorState = new GroundElevatorState();
+   }
+ 
+   public void Down(Elevator elevator)
+   {
+       Console.WriteLine("Опускаемся ещё ниже");
+   }
+}
+
+class GroundElevatorState : IElevatorState
+{
+   public void Up(Elevator elevator)
+   {
+       Console.WriteLine("Поднимаемся на верхний этаж");
+       elevator.ElevatorState = new UpperElevatorState();
+   }
+ 
+   public void Down(Elevator elevator)
+   {
+       Console.WriteLine("Опускаемся на подвальный этаж");
+       elevator.ElevatorState = new LowerElevatorState();
+   }
+}
+
+class UpperElevatorState : IElevatorState
+{
+   public void Up(Elevator elevator)
+   {
+       Console.WriteLine("Перемещаемся ещё выше");
+   }
+ 
+   public void Down(Elevator elevator)
+   {
+       Console.WriteLine("Опускаемся на первый этаж");
+       elevator.ElevatorState = new GroundElevatorState();
+   }
+}
+
+class Elevator {
+  /// Хранилище состояния
+  public IElevatorState ElevatorState {
+    get;
+    set;
+  }
+  public Elevator(IElevatorState elevatorState) {
+    ElevatorState = elevatorState;
+  }
+  // Подъем
+  public void Up() {
+    ElevatorState.Up(this);
+  }
+
+  // Спуск
+  public void Down() {
+    ElevatorState.Down(this);
+  }
+}
+
+
+   class Program
    {
        static void Main(string[] args)
        {
-           Receiver receiver = new Receiver(false, true, true);
-           
-           NotificationHandler emailNotificationHandler = new EmailNotificationHandler();
-           NotificationHandler voiceNotificationHandler = new VoiceNotificationHandler();
-           NotificationHandler smsNotificationHandler = new SmsNotificationHandler();
+           // инициализируем лифт (находится на земле)
+           Elevator elevator = new Elevator(new GroundElevatorState());
           
-           emailNotificationHandler.Successor = smsNotificationHandler;
-           smsNotificationHandler.Successor = voiceNotificationHandler;
-   
-           emailNotificationHandler.Handle(receiver);
+           elevator.Up(); // подъем наверх
+           elevator.Down(); // спуск на землю
+           elevator.Down(); // спуск в подвал
        }
    }
