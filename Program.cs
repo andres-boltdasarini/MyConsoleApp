513455822564d734755346209c977f41cd1d8f3c
akupriyanov <akupriyanov@astralinux.ru>
2025-06-10 10:24:47 +0300

    1741_Chain

--- Program.cs
+++ Program.cs
@@ -1,93 +1,64 @@
-﻿using System;
- 
-namespace FacadeRealExample.Components
-{
-   class Program
+﻿abstract class NotificationHandler {
+  public NotificationHandler Successor { get; set; }
+  public abstract void Handle(Receiver receiver);
+}
+
+class EmailNotificationHandler: NotificationHandler {
+  public override void Handle(Receiver receiver) {
+    if (receiver.EmailNotification == true)
+      Console.WriteLine("Выполнено уведомление по e-mail");
+    else if (Successor != null)
+      Successor.Handle(receiver);
+  }
+}
+
+class SmsNotificationHandler: NotificationHandler {
+  public override void Handle(Receiver receiver) {
+    if (receiver.SmsNotification == true)
+      Console.WriteLine("Выполнено уведомление по SMS");
+    else if (Successor != null)
+      Successor.Handle(receiver);
+  }
+}
+
+class VoiceNotificationHandler: NotificationHandler {
+  public override void Handle(Receiver receiver) {
+    if (receiver.VoiceNotification == true)
+      Console.WriteLine("Выполнено уведомление по телефону");
+    else if (Successor != null)
+      Successor.Handle(receiver);
+  }
+}
+
+   class Receiver
+   {
+       // банковские переводы
+       public bool EmailNotification { get; set; }
+       // денежные переводы - WesternUnion, Unistream
+       public bool SmsNotification { get; set; }
+       // перевод через PayPal
+       public bool VoiceNotification { get; set; }
+       public Receiver(bool emailNotification, bool smsNotification, bool voiceNotification)
+       {
+           EmailNotification = emailNotification;
+           SmsNotification = smsNotification;
+           VoiceNotification = voiceNotification;
+       }
+   }
+
+      class Program
    {
        static void Main(string[] args)
        {
-           // при запуске IDE инициализирует объекты для работы с компонентами
-           Editor textEditor = new Editor();
-           Compiller compiller = new Compiller();
-           Runtime runtime = new Runtime();
-       
-           // Наша модель IDE запущена и готова к использованию
-           IdeFacade ide = new IdeFacade(textEditor, compiller, runtime);
+           Receiver receiver = new Receiver(false, true, true);
+           
+           NotificationHandler emailNotificationHandler = new EmailNotificationHandler();
+           NotificationHandler voiceNotificationHandler = new VoiceNotificationHandler();
+           NotificationHandler smsNotificationHandler = new SmsNotificationHandler();
           
-           // Начинаем писать код и нажимаем кнопку Start
-           ide.Start("Console.WriteLine(\"Hello World!\");"); // запускается выполнение нашей программы
-           ide.Stop();
-       }
-   }
-
-   class Editor
-   {
-       // Функция написания кода
-       public void Write(string sourceCode)
-       {
-           Console.WriteLine($"Пишем код:  {sourceCode}");
-       }
-      
-       // Функция сохранения кода
-       public void Save()
-       {
-           Console.WriteLine("Сохраняем код");
-       }
-   }
-
-   class Compiller
-   {
-       public void Compile()
-       {
-           Console.WriteLine("Компиляция приложения");
-       }
-   }
-
-   class Runtime
-   {
-       public void Execute()
-       {
-           Console.WriteLine("Выполнение приложения");
-       }
-       public void Finish()
-       {
-           Console.WriteLine("Завершение работы приложения");
-       }
-   }
-
-      class IdeFacade
-   {
-       readonly Editor _editor;
-       readonly Compiller _compiler;
-       readonly Runtime _runtime;
-       public IdeFacade(Editor editor, Compiller compiler, Runtime runtime)
-       {
-           _editor = editor;
-           _compiler = compiler;
-           _runtime = runtime;
-       }
-      
-       // Вы ввели код, IDE выполняет с ним примерно следующие действия перед запуском:
-       public void Start(string sourceCode)
-       {
-           // Пишет в текстовый файл
-           _editor.Write(sourceCode);
-          
-           // Сохраняет текстовый файл
-           _editor.Save();
-          
-           // Вызывает компилятор
-           _compiler.Compile();
-          
-           // Запускает выполнение скомпилированного приложения в среде CLR
-           _runtime.Execute();
-       }
-      
-       // в конце IDE может остановить выполнение, вызвав команду в среде выполнения
-       public void Stop()
-       {
-           _runtime.Finish();
-       }
-   }
+           emailNotificationHandler.Successor = smsNotificationHandler;
+           smsNotificationHandler.Successor = voiceNotificationHandler;
    
-}
+           emailNotificationHandler.Handle(receiver);
+       }
+   }
